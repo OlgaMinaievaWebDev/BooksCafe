9cd3de03fe4adfff63e0a4f201e63c23c6016363
Olga <minaeva9@gmail.com>
2025-04-08 14:16:04 -0400

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
 function Footer() {
  return (
-  <div>
-   Foooo
-  </div>
+  <footer className="bg-gray-800 text-white text-center py-4">
+   <div className="flex justify-center space-x-4 mt-2">
+    <a href="#" className="text-gray-400 hover:text-white">Terms of Service</a>
+    <a href="#" className="text-gray-400 hover:text-white">Contact Us</a>
+   <p className="text-sm">Created by Your Name</p>
+    <a href="#" className="text-gray-400 hover:text-white">Careers</a>
+    <a href="#" className="text-gray-400 hover:text-white">About Us</a>
+
+   </div>
+   <p className="text-sm">© {} Book Store. All rights reserved.</p>
+   <div className="flex justify-center space-x-4 mt-2">
+    <a href="#" className="text-gray-400 hover:text-white">Facebook</a>
+    <a href="#" className="text-gray-400 hover:text-white">Twitter</a>
+    <a href="#" className="text-gray-400 hover:text-white">Instagram</a>
+   </div>
+ 
+  </footer>
  )
 }
 
