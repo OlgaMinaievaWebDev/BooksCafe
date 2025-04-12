d862ac237878e4945f354c23835310e2fd9acffa
Olga <minaeva9@gmail.com>
2025-04-12 12:41:24 -0400

    themeContext added to navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,13 @@ import { NavLink } from "react-router-dom"
 import SearchBar from "./SearchBar";
 import { FaMoon } from "react-icons/fa";
 import { FaSun } from "react-icons/fa";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 function Navbar() {
+
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
  return (
    <div className="bg-gray-800 text-white p-4 flex justify-between items-center">
      <h1 className="text-2xl font-bold">BooksCafe</h1>
@@ -22,9 +27,14 @@ function Navbar() {
        <NavLink to="/logout" className="mr-4 text-gray-300 hover:text-white">
          Logout
        </NavLink>
-       <button className="bg-amber-500 p-2 rounded-full hover:bg-amber-600 transition-colors duration-200 flex items-center justify-center shadow-md text-lg">
-         <FaMoon className="text-gray-300 hover:text-white" />
-         <FaSun className="text-gray-300 hover:text-white" />
+       <button
+         onClick={toggleTheme}
+         className="bg-amber-500 p-2 rounded-full hover:bg-amber-600 transition-colors duration-200 flex items-center justify-center shadow-md text-lg">
+         {theme === 'dark' ? (
+           <FaMoon className="text-gray-300 hover:text-white" />
+         ) : (
+           <FaSun className="text-gray-300 hover:text-white" />
+         )}
      </button>
      </nav>
    </div>
